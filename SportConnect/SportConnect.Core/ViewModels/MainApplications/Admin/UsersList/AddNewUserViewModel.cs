27bdac37b06dc5e008e79670ca06fa40616e922c
Kasper Cisewski <kaspercisewski@gmail.com>
2019-05-23 12:00:33 +0200

    code improvement

--- SportConnect/SportConnect.Core/ViewModels/MainApplications/Admin/UsersList/AddNewUserViewModel.cs
+++ SportConnect/SportConnect.Core/ViewModels/MainApplications/Admin/UsersList/AddNewUserViewModel.cs
@@ -1,15 +1,49 @@
-﻿using SportConnect.Core.Services.User;
+﻿using System;
+using System.Threading.Tasks;
+using Acr.UserDialogs;
+using MvvmCross.Commands;
+using SportConnect.Core.Services.Registration;
 using SportConnect.Core.ViewModels.Base;
 
 namespace SportConnect.Core.ViewModels.MainApplications.Admin.UsersList
 {
     public class AddNewUserViewModel : BaseViewModel
     {
-        private readonly UserService _userService;
+        private readonly IUserDialogs _userDialogs;
+        private readonly RegistrationService _registrationService;
 
-        public AddNewUserViewModel(UserService userService)
+        public AddNewUserViewModel(IUserDialogs userDialogs,
+            RegistrationService registrationService)
         {
-            _userService = userService;
+            _userDialogs = userDialogs;
+            _registrationService = registrationService;
+        }
+
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public IMvxAsyncCommand AddNewUserCommand =>
+           new MvxAsyncCommand(async () => { await RegisterNewUser(); });
+
+        private async Task RegisterNewUser()
+        {
+            if (!string.IsNullOrWhiteSpace(Email) &&
+                 !string.IsNullOrWhiteSpace(Login) &&
+                 !string.IsNullOrWhiteSpace(Password))
+            {
+                var isRegistered = await _registrationService.TryToRegisterUserAsync(new RegistrationResponseApiModel(Login, Email, Password));
+
+                if (isRegistered)
+                {
+                    _userDialogs.Alert(new AlertConfig() { Message = "You register into app" });
+                    await NavigationService.Close(this);
+                }
+                else
+                {
+                    _userDialogs.Alert(new AlertConfig() { Message = "Sorry, we can't register you" });
+                }
+            }
         }
     }
 }
